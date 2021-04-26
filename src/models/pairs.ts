960a7a2075a877aa61950cfed6bcab7e37f7dc63
LeoSL <leosilvalima@gmail.com>
2021-04-26 02:32:00 -0400

    Centralize typing

--- src/models/pairs.ts
+++ src/models/pairs.ts
@@ -5,3 +5,32 @@ export enum TicketPairEnum {
   "BTC-USD" = "BTCUSD",
   "ETH-CAD" = "ETHCAD",
 }
+
+type Order = {
+  price: string;
+};
+
+export type Orderbook = {
+  asks: Order[];
+  bids: Order[];
+};
+
+export type PriceAggregate = {
+  high: string;
+  low: string;
+  close: string;
+  average: string;
+  percentChange: string;
+  orderbook: Orderbook;
+};
+
+export type MarketDataResponse = {
+  displaySymbol: string;
+  baseCurrency: string;
+  quoteCurrency: string;
+  marketDataLast24HourPriceAggregate: PriceAggregate;
+};
+
+export type MarketData = {
+  marketDataResponse: MarketDataResponse;
+};
